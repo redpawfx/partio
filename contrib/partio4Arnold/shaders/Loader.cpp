1c761ca5ef02f6bc64adc7b7dbdc6bd4f19f7271
redpawfx <redpawfx@gmail.com>
2015-03-29 23:54:30 -0700

    updated  partioPoint shader to add incandescencePP support for emission.

--- contrib/partio4Arnold/shaders/Loader.cpp
+++ contrib/partio4Arnold/shaders/Loader.cpp
@@ -5,24 +5,24 @@ extern AtNodeMethods* partioPointMtd;
 
 enum SHADERS
 {
-	SHADER_PARTIOPOINT
+    SHADER_PARTIOPOINT
 };
 
 node_loader
 {
-   switch (i)
-   {
-	case SHADER_PARTIOPOINT:
-		node->methods     = (AtNodeMethods*) partioPointMtd;
-		node->output_type = AI_TYPE_RGBA;
-		node->name        = "partioPoint";
-		node->node_type   = AI_NODE_SHADER;
-		break;
-	default:
-		return false;
-   }
+	switch(i)
+	{
+		case SHADER_PARTIOPOINT:
+			node->methods     = (AtNodeMethods*) partioPointMtd;
+			node->output_type = AI_TYPE_RGBA;
+			node->name        = "partioPoint";
+			node->node_type   = AI_NODE_SHADER;
+			break;
+		default:
+			return false;
+	}
 
-   sprintf(node->version, AI_VERSION);
+	sprintf(node->version, AI_VERSION);
 
-   return true;
+	return true;
 }
--- contrib/partio4Arnold/shaders/partioPoint.cpp
+++ contrib/partio4Arnold/shaders/partioPoint.cpp
@@ -6,69 +6,77 @@ AI_SHADER_NODE_EXPORT_METHODS(partioPointMtd);
 
 enum partioPointParams
 {
-	p_preMult,
-	p_overrideRgbPP,
-	p_particleColor,
-	p_overrideOpacityPP,
-	p_particleOpacity,
-	p_blackHole,
-	p_MatteOpacity,
-	p_doShadowOpacity,
-	p_useLighting,
-//	p_Ka,
-	p_Kd,
-	p_Ki,
-	p_Ks,
-	p_shiny,
-	p_KOcc,
-	p_occSamples,
-	p_occSpread,
-	p_occFalloff,
-	p_occMinDist,
-	p_occMaxDist,
-	p_invertOcc,
-	p_emission,
-	p_aov_emission
+    p_preMult,
+    p_overrideRgbPP,
+    p_particleColor,
+    p_overrideOpacityPP,
+    p_particleOpacity,
+    p_blackHole,
+    p_MatteOpacity,
+    p_doShadowOpacity,
+    p_useLighting,
+    //p_Ka,
+    p_Kd,
+    p_Ki,
+    p_Ks,
+    p_shiny,
+    p_KOcc,
+    p_occSamples,
+    p_occSpread,
+    p_occFalloff,
+    p_occMinDist,
+    p_occMaxDist,
+    p_invertOcc,
+    p_emFromIncand,
+    p_emission,
+    p_aov_direct_diffuse,
+    p_aov_indirect_diffuse,
+    p_aov_emission,
+
+
 };
 
 
 node_parameters
 {
-	AiParameterBOOL ( "premult"				, false);
-    AiParameterBOOL ( "overrideRgbPP"		, false);
-    AiParameterRGB	( "particleColor"		, 1.0f, 1.0f, 1.0f);
-    AiParameterBOOL ( "overrideOpacityPP"	, false);
-    AiParameterRGB	( "particleOpacity"		, 1.0f, 1.0f, 1.0f);
-	AiParameterBOOL ( "blackHole"			, false);
-    AiParameterRGB	( "matteOpacity"		, 1.0f, 1.0f, 1.0f);
-	AiParameterBOOL ( "doShadowOpacity"     , false);
-    AiParameterBOOL	( "useLighting"			, true);
+	AiParameterBOOL("premult"				, false);
+	AiParameterBOOL("overrideRgbPP"		, false);
+	AiParameterRGB("particleColor"		, 1.0f, 1.0f, 1.0f);
+	AiParameterBOOL("overrideOpacityPP"	, false);
+	AiParameterRGB("particleOpacity"		, 1.0f, 1.0f, 1.0f);
+	AiParameterBOOL("blackHole"			,false);
+	AiParameterRGB("matteOpacity"		, 1.0f, 1.0f, 1.0f);
+	AiParameterBOOL("doShadowOpacity"     ,false);
+	AiParameterBOOL("useLighting"			,true);
 //    AiParameterFLT	( "Ka"	              	, 0.1f );  // AiAmbient has been removed  from arnold 4.1.+
-    AiParameterFLT	( "Kd"                	, 0.8f );
-    AiParameterFLT  ( "Ki"					, 1.0f );
-    AiParameterFLT	( "Ks"					, 1.0f );
-	AiParameterFLT  ( "shiny"				, 10.0f );
-    AiParameterFLT	( "KOcc"				, 1.0f);
-	AiParameterINT 	( "occSamples"          , 5 );
-    AiParameterFLT	( "occSpread"			, 90 );
-    AiParameterFLT	( "occFalloff"			, 0 );
-    AiParameterFLT	( "occMinDist"			, 0 );
-    AiParameterFLT	( "occMaxDist"			, 2 );
-	AiParameterBool ( "invertOcc"			, false);
-    AiParameterRGB  ( "emission"      		, 0.0f,0.0f,0.0f);
-    AiParameterSTR  ( "aov_emission"        ,"emission");
+	AiParameterFLT("Kd"                	, 0.8f);
+	AiParameterFLT("Ki"					, 1.0f);
+	AiParameterFLT("Ks"					, 1.0f);
+	AiParameterFLT("shiny"				, 10.0f);
+	AiParameterFLT("KOcc"				, 1.0f);
+	AiParameterINT("occSamples"          , 5);
+	AiParameterFLT("occSpread"			, 90);
+	AiParameterFLT("occFalloff"			, 0);
+	AiParameterFLT("occMinDist"			, 0);
+	AiParameterFLT("occMaxDist"			, 2);
+	AiParameterBool("invertOcc"			,false);
+	AiParameterBool("emColorFromIncan"	,false);
+	AiParameterRGB("emission"      		, 0.0f,0.0f,0.0f);
+	AiParameterSTR("aov_direct_diffuse"  ,"direct_diffuse");
+	AiParameterSTR("aov_indirect_diffuse" ,"indirect_diffuse");
+	AiParameterSTR("aov_emission"        ,"emission");
 
 
-    AiMetaDataSetStr(mds, NULL, "maya.classification", "shader/surface");
-    AiMetaDataSetStr(mds, NULL, "maya.name", "partioPoint");
+	AiMetaDataSetStr(mds, NULL, "maya.classification", "shader/surface");
+	AiMetaDataSetStr(mds, NULL, "maya.name", "partioPoint");
 	//AiMetaDataSetInt(mds, NULL, "maya.id", NULL);
 }
 
 enum AutoTransparency
 {
-   ALWAYS,
-   SHADOW_ONLY,
-   NEVER
+    ALWAYS,
+    SHADOW_ONLY,
+    NEVER
 };
 
 typedef struct
@@ -79,8 +87,8 @@ ShaderData;
 
 node_initialize
 {
-  ShaderData *data = (ShaderData*) AiMalloc(sizeof(ShaderData));
-  AiNodeSetLocalData(node, data);
+	ShaderData *data = (ShaderData*) AiMalloc(sizeof(ShaderData));
+	AiNodeSetLocalData(node, data);
 }
 
 ///////////////////////////////////////
@@ -88,8 +96,8 @@ node_initialize
 node_update
 {
 	ShaderData *data = (ShaderData*)AiNodeGetLocalData(node);
-    data->sampler = NULL;
-    data->sampler = AiSampler(AiNodeGetInt(node, "occSamples"), 2);
+	data->sampler = NULL;
+	data->sampler = AiSampler(AiNodeGetInt(node, "occSamples"), 2);
 }
 
 
@@ -97,10 +105,10 @@ node_update
 /// FINISH
 node_finish
 {
-    ShaderData *data = (ShaderData*)AiNodeGetLocalData(node);
-    AiSamplerDestroy(data->sampler);
-    //Freeing ShaderData
-    AiFree(data);
+	ShaderData *data = (ShaderData*)AiNodeGetLocalData(node);
+	AiSamplerDestroy(data->sampler);
+	//Freeing ShaderData
+	AiFree(data);
 }
 
 shader_evaluate
@@ -114,6 +122,8 @@ shader_evaluate
 
 	AtRGB 		diffuse, indirDiff, ambient, occ, spec;
 	AtRGB		rgbPP;
+	AtRGB		incand;
+	AtVector    incandVec;
 	float 		opacityPP;
 	float		minDist, maxDist, spread, falloff;
 	AtRGB 		weighted_sample;
@@ -130,27 +140,41 @@ shader_evaluate
 	if(!AiShaderEvalParamBool(p_doShadowOpacity))
 	{
 		// early out shadow rays
-		if (sg->Rt & AI_RAY_SHADOW)
+		if(sg->Rt & AI_RAY_SHADOW)
 			return;
 	}
 
 	AtColor emission_color = AiShaderEvalParamRGB(p_emission);
-	if (AiColorToGrey(emission_color) != 0.0f)
+	if(AiUDataGetVec("incandescencePP", &incandVec) && AiShaderEvalParamBool(p_emFromIncand))
 	{
-		Ci += emission_color;
+		AtColor temp;
+		temp.r = incandVec.x;
+		temp.g = incandVec.y;
+		temp.b = incandVec.z;
+		emission_color = temp * emission_color; // used in this case as a multiplier
+	}
+	else if(AiUDataGetRGB("incandescencePP", &incand) && AiShaderEvalParamBool(p_emFromIncand))
+	{
+		emission_color = incand * emission_color;
+	}
+
+	AiColorClipToZero(emission_color);
+
+	if(AiColorToGrey(emission_color) != 0.0f)
+	{
 		AiAOVSetRGB(sg, AiShaderEvalParamStr(p_aov_emission), emission_color);
 	}
 
-	if (AiUDataGetRGB("rgbPP", &rgbPP) &&!AiShaderEvalParamBool(p_overrideRgbPP))
-    {
-    }
+	if(AiUDataGetRGB("rgbPP", &rgbPP) &&!AiShaderEvalParamBool(p_overrideRgbPP))
+	{
+	}
 	else
 	{
 		rgbPP = AiShaderEvalParamRGB(p_particleColor);
 	}
 	Ci += rgbPP;
 
-	if (AiUDataGetFlt("opacityPP", &opacityPP) && !AiShaderEvalParamBool(p_overrideOpacityPP))
+	if(AiUDataGetFlt("opacityPP", &opacityPP) && !AiShaderEvalParamBool(p_overrideOpacityPP))
 	{
 		Oi.r = opacityPP;
 		Oi.g = opacityPP;
@@ -161,22 +185,25 @@ shader_evaluate
 		Oi = AiShaderEvalParamRGB(p_particleOpacity);
 	}
 
-	if (AiShaderEvalParamBool(p_useLighting))
+	if(AiShaderEvalParamBool(p_useLighting))
 	{
 		AiColorReset(spec);
 		//ambient = AiAmbient; // AiAmbient removed from arnold  4.1+
 
-		diffuse = AiDirectDiffuse (&sg->Nf, sg);
+		diffuse = AiDirectDiffuse(&sg->Nf, sg);
 		indirDiff = AiIndirectDiffuse(&sg->Nf, sg);
 
-		if (AiShaderEvalParamFlt(p_Ks) > 0)
+		AiAOVSetRGB(sg, AiShaderEvalParamStr(p_aov_direct_diffuse), diffuse);
+		AiAOVSetRGB(sg, AiShaderEvalParamStr(p_aov_indirect_diffuse), indirDiff);
+
+		if(AiShaderEvalParamFlt(p_Ks) > 0)
 		{
 			float shinyVal = AiShaderEvalParamFlt(p_shiny);
 			float shiny = CLAMP((shinyVal*shinyVal), 2.0f , 100000.0f);
 			AiV3Neg(V, sg->Rd);
 			brdf = AiStretchedPhongBRDF(&sg->Ld, &V, &sg->Nf,shiny);
-        	AiColorScale(weighted_sample, sg->Li, sg->we * brdf);
-        	AiColorAdd(spec, spec, weighted_sample);
+			AiColorScale(weighted_sample, sg->Li, sg->we * brdf);
+			AiColorAdd(spec, spec, weighted_sample);
 			spec *= AiShaderEvalParamFlt(p_Ks);
 		}
 
@@ -190,9 +217,8 @@ shader_evaluate
 		//AiColorAdd(lighting, diffuse,ambient);
 		AiColorAdd(lighting, diffuse, indirDiff);
 
-
 		AiColorMult(Ci,Ci,lighting);
-		if (AiShaderEvalParamFlt(p_Ks))
+		if(AiShaderEvalParamFlt(p_Ks))
 		{
 			AiColorAdd(Ci, Ci, spec);
 		}
@@ -200,26 +226,28 @@ shader_evaluate
 
 	if(AiShaderEvalParamFlt(p_KOcc) > 0)
 	{
-		float  radians = CLAMP(spread, 0.0f,90.0f) *  (float)AI_DTOR;
+		float  radians = CLAMP(spread, 0.0f,90.0f) * (float)AI_DTOR;
 		AtVector NBent;
-		occ = AiOcclusion (&sg->Nf,&sg->Ngf, sg, minDist,maxDist,radians,falloff,data->sampler,&NBent);
- 		occ *= AiShaderEvalParamFlt(p_KOcc);
+		occ = AiOcclusion(&sg->Nf,&sg->Ngf, sg, minDist,maxDist,radians,falloff,data->sampler,&NBent);
+		occ *= AiShaderEvalParamFlt(p_KOcc);
 
-		if (!AiShaderEvalParamBool(p_invertOcc))  // normal values of occlusion are flipped  so allow us to use either one..
+		if(!AiShaderEvalParamBool(p_invertOcc))   // normal values of occlusion are flipped  so allow us to use either one..
 		{
 			occ.r = 1-occ.r;
 			occ.g = 1-occ.g;
 			occ.b = 1-occ.b;
 		}
 		AiColorClipToZero(occ);
-		if (!AiColorCorrupted(occ))
+		if(!AiColorCorrupted(occ))
 		{
 			AiColorMult(Ci, Ci, occ);
 		}
 
 	}
 
-	if (!blackHole)
+	AiColorAdd(Ci, Ci, emission_color);
+
+	if(!blackHole)
 	{
 		AiColorClipToZero(Oi);
 		Oi *= AiColorToGrey(MatOpac);
@@ -231,7 +259,7 @@ shader_evaluate
 		Ci *= AI_RGB_BLACK;
 	}
 
-	if (AiShaderEvalParamFlt(p_preMult))
+	if(AiShaderEvalParamFlt(p_preMult))
 	{
 		Ci *= AiColorToGrey(Oi);
 	}
@@ -239,10 +267,10 @@ shader_evaluate
 ///////////////////////////
 /// FINAL OUTPUT!
 
-sg->out.RGBA.r = Ci.r;
-sg->out.RGBA.g = Ci.g;
-sg->out.RGBA.b = Ci.b;
-sg->out.RGBA.a = AiColorToGrey(Oi);
+	sg->out.RGBA.r = Ci.r;
+	sg->out.RGBA.g = Ci.g;
+	sg->out.RGBA.b = Ci.b;
+	sg->out.RGBA.a = AiColorToGrey(Oi);
 
 }
 
